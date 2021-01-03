b92e4e5503790cd89bf58749904637bc6fdc6bc2
burcu macit <74739990+burcumacit@users.noreply.github.com>
2021-01-03 20:56:02 +0300

    Add files via upload

---
+++ src/core/OyunNesnesi/OyunNesnesi.java
@@ -0,0 +1,13 @@
+package com.mygdx.OyunNesnesi;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public interface OyunNesnesi {
+	public void render(SpriteBatch sb);
+	public void update(float delta);
+	
+	
+	
+	
+
+}
