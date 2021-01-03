398c70eb5030528aa2a805cde5e6e13a870ad441
NUR GÜLSEN <74740005+nur-glsen187@users.noreply.github.com>
2021-01-03 21:14:51 +0300

    Add files via upload

---
+++ src/core/states/MenuDurum.java
@@ -0,0 +1,25 @@
+package com.mygdx.states;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MenuDurum extends durum{
+
+	@Override
+	public void render(SpriteBatch sb) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(float delta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void handleGiris() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
